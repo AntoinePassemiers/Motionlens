12305ed8118d78ef6fb4ecf6def48a4ccf3b2cba
AntoinePassemiers <apassemi@ulb.ac.be>
2017-08-15 18:07:50 +0200

    Using sensors to display data

--- Client/Motionlens/app/src/main/java/com/example/motionlens/motionlens/MainActivity.java
+++ Client/Motionlens/app/src/main/java/com/example/motionlens/motionlens/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.motionlens.motionlens;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +14,25 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private static boolean USE_WAKE_LOCK = true;
+
+    private boolean recording = false;
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer, gyroscope;
+    private Context context;
+    private PowerManager.WakeLock wakeLock;
+
+    private TextView currentX, currentY, currentZ;
+    private TextView gyroscopeX, gyroscopeY, gyroscopeZ;
+    private Button recordButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        context = getApplicationContext();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +50,58 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // https://developer.android.com/training/keyboard-input/style.html
+        AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.activity_with_white_list);
+        String[] human_activities = getResources().getStringArray(R.array.human_activities);
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, human_activities);
+        textView.setAdapter(adapter);
+
+        currentX = (TextView) findViewById(R.id.currentX);
+        currentY = (TextView) findViewById(R.id.currentY);
+        currentZ = (TextView) findViewById(R.id.currentZ);
+        gyroscopeX = (TextView) findViewById(R.id.gyroscopeX);
+        gyroscopeY = (TextView) findViewById(R.id.gyroscopeY);
+        gyroscopeZ = (TextView) findViewById(R.id.gyroscopeZ);
+
+        recordButton = (Button) findViewById(R.id.record_button);
+        recordButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                recording = !recording;
+                if (recording) {
+                    recordButton.setText("Pause");
+                }
+                else {
+                    recordButton.setText("Record");
+                }
+            }
+        });
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+            sensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        else {
+            // TODO: raise error
+        }
+        if (USE_WAKE_LOCK) {
+            acquireWakeLock();
+        }
+    }
+
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
     }
 
     @Override
@@ -48,5 +124,38 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor == accelerometer) {
+            currentX.setText(Float.toString(event.values[0]));
+            currentY.setText(Float.toString(event.values[1]));
+            currentZ.setText(Float.toString(event.values[2]));
+        }
+        else if (event.sensor == gyroscope) {
+            gyroscopeX.setText(Float.toString(event.values[0]));
+            gyroscopeY.setText(Float.toString(event.values[1]));
+            gyroscopeZ.setText(Float.toString(event.values[2]));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // TODO
+    }
+
+    public void acquireWakeLock() {
+        final PowerManager powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        releaseWakeLock();
+        wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "PARTIAL_WAKE_LOCK");
+        wakeLock.acquire();
+    }
+
+    public void releaseWakeLock() {
+        if (wakeLock != null && wakeLock.isHeld()) {
+            wakeLock.release();
+            wakeLock = null;
+        }
     }
 }
