fa63736cc636d4a797f1e1e9fd0c0ffa1a4236dd
renavspainatal <renavspainatal@hotmail.com>
2017-09-12 23:23:22 +0200

    [Server] First working version

--- Server/app.py
+++ Server/app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, request
+from werkzeug.utils import secure_filename
+import os
+
+ROOT_FOLDER = os.getcwd()
+UPLOAD_FOLDER = "files"
+ALLOWED_EXTENSIONS = {"bonjour"}
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-	return "test"
+@app.route("/file", methods = ["POST"])
+def upload():
+	f = request.files["data"]
+	secureFilename = secure_filename(f.filename)
+	filePath = os.path.join(ROOT_FOLDER,UPLOAD_FOLDER,secureFilename)
+	#secureFilePath = secure_filename(filePath)
+	print("________",filePath)
+	f.save(filePath)
+	return str({"yo":"ok"})
 
 if __name__ == "__main__":
 	app.run(debug = True)
